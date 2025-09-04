ccdc7d91842954cb6336a3bacd2938ee677e393c
ClarktTion <tionclarkdavid@gmail.com>
2025-09-04 08:47:11 +0800

    Added interface and parameter for menu choice and conditional statement for the selected choice

--- factorial.js
+++ factorial.js
@@ -5,9 +5,20 @@ const rl = readline.createInterface({
     output : process.stdout
 })
 
-
 console.log ("Factorial Application");
 
 console.log ("1. Say Hello");
 console.log ("2. Compute factorial");
 console.log ("3. Exit")
+
+rl.question("Enter your choice (1-3)", choice =>{
+    if (choice=== "1"){
+        console.log ("Hello There!");
+    } else if (choice === "2"){
+        console.log("Factorial");
+    }else if (choice === "3"){
+        console.log ("Exit");
+    }else{
+        console.log("Invalid choice. Please Try Again");
+    }
+})
