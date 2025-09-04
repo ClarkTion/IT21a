5c77a82323fe1d8773b01090939966a1e6c22ccd
ClarktTion <tionclarkdavid@gmail.com>
2025-09-04 08:32:55 +0800

    Added choices and readline interface

--- factorial.js
+++ factorial.js
@@ -1,1 +1,13 @@
-console.log("Factorial Application");
+const readline = require ("readline");
+
+const rl = readline.createInterface({
+    input : process.stdin,
+    output : process.stdout
+})
+
+
+console.log ("Factorial Application");
+
+console.log ("1. Say Hello");
+console.log ("2. Compute factorial");
+console.log ("3. Exit")
